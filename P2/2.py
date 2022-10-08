759b4527263c7a84fbe1e2d4ce49944acaeab5a9
julisahun <juli.sahun@estudiantat.upc.edu>
2022-10-08 09:24:16 +0200

    div not working propertly

--- P2/2.py
+++ P2/2.py
@@ -3,7 +3,7 @@ import copy
 m = [1,1,0,0,0,1,0,1,1]
 
 class Poly:
-    def __init__(self, params =[0,0,0,0,0,0,0,0]):
+    def __init__(self, params =[0,0,0,0,0,0,0,0,0]):
         self.params = params
 
     def toPoly(list):
@@ -21,35 +21,32 @@ class Poly:
         self.params.append(0)
         val = self.params.pop(0)
         if val == 1:
-            self.add(Poly([1,0,0,0,1,0,1,1]))
+            self.add(Poly([0,1,0,0,0,1,0,1,1]))
 
     def mult(self,poly):
         if type(poly) == Poly:
             list = poly.params
         else:
             list = poly
-        acc = Poly([0,0,0,0,0,0,0,0])
+        acc = Poly([0,0,0,0,0,0,0,0,0])
         for indx, val in enumerate(list):
             if val == 0: continue
-            original = self
-            pow = 7-indx
+            original = copy.deepcopy(self)
+            pow = 8-indx
             while pow > 0:
                 original.multX()
                 pow -= 1
             acc.add(original)
-        self = acc
-    
-    def div(self, poly):
-        if self.lte(poly):
-            return 
+        self.params = acc.params
+     
 
-
-    def gte(self, poly):        
+    def gte(self, poly):       
         for i, val in enumerate(poly.params):
-            if self.params[i] < val:
-                return False
-            if self.params[i] > val:
+            if val == 1:
+                return self.params[i] >= val
+            elif self.params[i] == 1:
                 return True
+
         return True
 
     def lte(self, poly):
@@ -69,17 +66,17 @@ class Poly:
     def next(self):
         for indx, val in enumerate(self.params[::-1]):
             if val == 0:
-                self.params[7-indx] = 1
+                self.params[8-indx] = 1
                 return 
-            else: self.params[7-indx] = 0
+            else: self.params[8-indx] = 0
 
     def prev(self):
         for indx, val in enumerate(self.params[::-1]):
             
             if val == 1:
-                self.params[7-indx] = 0
+                self.params[8-indx] = 0
                 return 
-            else: self.params[7-indx] = 1
+            else: self.params[8-indx] = 1
                 
     def print(self):
         print(self.params)
@@ -90,6 +87,29 @@ def toPoly(poly):
     else:
         a = copy.deepcopy(Poly(poly))
     return a
+
+def numToPoly(num):
+    pow = 256
+    poly = []
+    while pow != 0:
+        val = 0
+        if num >= pow:
+            num = num - pow
+            val = 1
+        poly.append(val)
+        pow = pow // 2
+    return poly
+
+def polyToNum(poly):
+    pow = 256
+    num = 0
+    for val in poly:
+        if val == 1: 
+            num = num + pow
+            pow = pow // 2
+    return num
+    
+        
 
 def mult(poly1, poly2):
     a = toPoly(poly1)
@@ -102,9 +122,11 @@ def add(poly1, poly2):
     return a
 
 def div(a, b):
-    q = Poly([0,0,0,0,0,0,0,1])
+    #0010
+    q = Poly([0,0,0,0,0,0,0,0,1])
     while a.gte(mult(q,b)):
         q.next()
+        mult(q,b).print()
     q.prev()
     if a.eq(mult(q,b)):
         return [q, Poly()]
@@ -113,56 +135,50 @@ def div(a, b):
 #Nota, Les funcions de la classe Poly, modifiquen l'objecte de la classe, 
 #      Les funcions del modul, no modifiquen cap dels objectes dels parametres
 
-a = Poly([0,0,0,0,0,1,0,0])
-b = Poly([0,0,0,0,0,0,1,0])
+a = Poly([0,0,0,0,0,0,1,0,1])
+b = Poly([0,0,0,0,0,0,1,1,1])
 
+print(a.gte(b))
 res = div(a,b)
 print('resultat:')
 res[0].print()
 res[1].print()
+
+# print(numToPoly(471))
+
+
+# entrada: a y b elementos del cuerpo representados por enteros entre 0 y 255;
+# salida: un elemento del cuerpo representado por un entero entre 0 y 255 que es el
+# producto en el cuerpo de a y b calculado usando la definici´on en t´erminos de
+# polinomios.
+def GF_product_p(a, b):
+    return polyToNum(mult(numToPoly(a), numToPoly(b)))
     
-# def irreductible4():
-#     irreduct = []
-#     first = True
-#     for i5 in [0,1]:
-#         for i4 in [0,1]:
-#             for i3 in [0,1]:
-#                 for i2 in [0,1]:
-#                     for i1 in [0,1]:
-#                         if first: 
-#                             first = False
-#                             continue
-#                         isIrr = True
-#                         for poly in irreduct:
-#                             a = np.poly1d([0,0,0,i5,i4, i3, i2, i1])
-#                             b = np.poly1d(poly)
-#                             print(a, b, np.polydiv(a,b))
-#                             if np.polydiv(a,b)[1][0] == 0 and np.polydiv(a,b)[0] != a:
-#                                 isIrr = False
-#                                 break
-#                         if isIrr:
-#                             irreduct.append([0,0,0,i5,i4, i3, i2, i1])
-#     return irreduct
 
-# irreduct4 = irreductible4()
-# print(irreduct4)
-# irreduct = []
-# for i7 in [0,1]:
-#     for i6 in [0,1]:
-#         for i5 in [0,1]:
-#             for i4 in [0,1]:
-#                 for i3 in [0,1]:
-#                     for i2 in [0,1]:
-#                         for i1 in [0,1]:
-#                             for i0 in [0,1]:
-#                                 a = np.poly1d([1,i7,i6,i5,i4,i3,i2,i1,i0])
-#                                 isIrr = True    
-#                                 for poly in irreduct4:
-#                                     b = np.poly1d(poly)
-#                                     if np.polydiv(a,b)[1][0] == 0 and np.polydiv(a,b)[0] != a:
-#                                         isIrr = False
-#                                         break
-#                                 if isIrr:
-#                                     irreduct.append([1,i7,i6,i5,i4,i3,i2,i1,i0])
-# print(irreduct, len(irreduct))
+# entrada: a elemento del cuerpo representado por un entero entre 0 y 255;
+# salida: True si a es generador del cuerpo, False si no lo es.
+def GF_es_generador(a):
+    return 0
 
+# entrada:
+# salida: dos tablas (exponencial y logaritmo), la primera tal que en la posici´on i tenga
+# a = g
+# i y la segunda tal que en la posici´on a tenga i tal que a = g
+# i
+# . (g generador
+# del cuerpo finito representado por el menor entero entre 0 y 255.)
+# 3
+def GF_tables():
+    return 0
+
+# entrada: a y b elementos del cuerpo representados por enteros entre 0 y 255;
+# salida: un elemento del cuerpo representado por un entero entre 0 y 255 que es el
+# producto en el cuerpo de a y b calculado usando las tablas exponencial y
+# logaritmo.
+def GF_product_t(a, b):
+    return 0
+
+# entrada: a elemento del cuerpo representado por un entero entre 0 y 255;
+# salida: 0 si a=0x00, in
+def GF_invers(a):
+    return 0

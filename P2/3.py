9bc57256a97b9ee03b9159a728ecc23909ca8774
juli.sahun <juli.sahun@estudiantat.upc.edu>
2022-10-12 17:13:42 +0200

    3 fixed (for real)

--- P2/3.py
+++ P2/3.py
@@ -4,6 +4,8 @@ from Crypto.Cipher import AES
 import functools
 import filetype
 import hashlib
+from IPython.display import HTML
+from base64 import b64encode
 
 name = "juli"
 
@@ -21,16 +23,10 @@ print(filetype.guess(pt))
 # b
 
 
-def listXor(a, b):
-    ret = []
-    for i in range(len(a)):
-        ret.append(a[i] ^ b[i])
-    return ret
-
 def toBinary(list):
     ret = bytearray()
     for i in list:
-        ret.append(i)
+      ret = ret + chr(i).encode('utf-8')
     return bytes(ret)
 
 def pad(key):
@@ -40,25 +36,37 @@ def pad(key):
         ret.append(p)
     return bytes(ret)
 
-
+name = "juli"
 ciphertext = open("./files/" + name + "/AES_juli.sahun_2022_09_20_16_59_27.puerta_trasera.enc", "rb").read()
 files = {}
-for i in range(256):
+for i in range(96, 97):
     print(i)
     for j in range(256):
-        h = hashlib.sha256(toBinary([i,i,i,i,i,i,i,i,j,j,j,j,j,j,j,j])).hexdigest()
-        finalKey = pad(str.encode(h[:16]))
+        a = toBinary([i,i,i,i,i,i,i,i,j,j,j,j,j,j,j,j])
+        h = hashlib.sha256(a).digest()
+        finalKey = h[:16]
 
-        iv = str.encode(h[48:])
+        iv = h[16:]
 
         cipher = AES.new(finalKey, AES.MODE_CBC, iv)
         pt = cipher.decrypt(ciphertext)
-        if filetype.guess(pt) != None:
-            print(filetype.guess(pt))
-            if filetype.guess(pt) not in files:
-                files[filetype.guess(pt)] = []
-            files[filetype.guess(pt)].append(pt)
-                                                                   
+        t = str(filetype.guess(pt))
+        if t != 'None':
+            print(t)
+            if t not in files:
+                files[t] = []
+            files[t].append(pt)
+
+video = None
+for key in files.keys():
+  if 'Mp4' in key:
+    video = files[key][0]
+data_url = "data:video/mp4;base64," + b64encode(video).decode()
+HTML("""
+<video width=400 controls>
+      <source src="%s" type="video/mp4">
+</video>
+""" % data_url)                                   
             
 
 

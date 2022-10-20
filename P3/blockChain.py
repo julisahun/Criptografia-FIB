a080febee9a7ec649f78e99d8e115188c77dbc71
julisahun <julisahun@gmail.com>
2022-10-20 10:51:57 +0200

    apunts

--- P3/blockChain.py
+++ P3/blockChain.py
@@ -27,3 +27,27 @@ class rsa_public_key:
     def __init__(self):
         self.publicExponent
         self.modulus
+
+from sympy import gcdex, mod_inverse
+import Math
+
+# d = gcdex(2, 1)[0]
+
+#firma
+# s = m**d % n -> pow(m, d % phi, n) 
+
+
+
+##teorema chino
+# X = x mod (p*q)
+# a = x mod p
+# b = x mod q
+# p' = mod_inverse(p, q)
+# q' = mod_inverse(q, p)
+# pp' + qq' = 1
+# X = p*p'*b + q*q'*a
+
+#aplicado a la firma
+# s = m**d % n     n = q*p
+# a = m**(d % p-1) % p
+# b = m**(d % q-1) % q    

17088044db8596f606c6d3b290987e94e1695de2
Victor Pla <vplasanchis@gmail.com>
2022-10-25 19:30:49 +0200

    buscar error + verify rsa_public_key
    He fet un metode per anar verificant els congruents, sembla que tots els parametres de rsa_key son correctes, pero al moment de fer el sign no funca, si es verifica el sign amb el message, tant sign_slow com sign amb TCR no es True... Te pinta que fem overflow o algo, no entenc com es destrossa tant el misatge a lhora de fer la firma

--- P3/blockChain.py
+++ P3/blockChain.py
@@ -17,6 +17,16 @@ class transaction:
         self.signature
 
 class rsa_key:
+
+    def check_congruent(self,a,c,m,debug=True):
+        if (a % m) != (c % m):
+            print("Congruent check not passed.")
+            exit(1)
+        else:
+            if debug:
+                print("Congruent check passed.")
+            return True
+
     def __init__(self, bits_modulo=2048, e=2**16+1):
         self.primeP, self.primeQ =  self.getPrimes(e, bits_modulo)
         self.phi = (self.primeP-1)*(self.primeQ-1)
@@ -26,6 +36,11 @@ class rsa_key:
         self.privateExponentModulusPhiP = int(self.privateExponent % (self.primeP-1))
         self.privateExponentModulusPhiQ = int(self.privateExponent % (self.primeQ-1))
         self.inverseQModulusP = int(sympy.mod_inverse(self.primeQ, self.primeP))
+
+        self.check_congruent(self.privateExponent * self.publicExponent, 1, self.phi,debug=False)
+        self.check_congruent(self.privateExponentModulusPhiP, self.privateExponent, self.primeP-1,debug=False)
+        self.check_congruent(self.privateExponentModulusPhiQ, self.privateExponent, self.primeQ-1,debug=False)
+        self.check_congruent(self.inverseQModulusP * self.primeQ, 1, self.primeP,debug=False)
     
     def sign(self, message):
         a = pow(message, self.privateExponentModulusPhiP, self.primeP)
@@ -50,14 +65,19 @@ class rsa_key:
             if  math.gcd((p-1)*(q-1), e) == 1:
                 return int(p), int(q)
 
-
 class rsa_public_key:
-    def __init__(self):
-        self.publicExponent
-        self.modulus
+    def __init__(self):    # rsa_key: {e,m}
+        self.publicExponent = 2**16+1
+        self.modulus = 2048
+
+    def verify(self, message, signature):
+        return pow(signature,self.publicExponent,self.modulus) == message
 
 a = rsa_key()
-print(a.sign(123) - a.sign_slow(123))
+b = rsa_public_key()
+print(b.verify(123, a.sign(123)))
+print(b.verify(123, a.sign_slow(123)))
+#print(a.sign(123) == a.sign_slow(123))
 
 # d = gcdex(e, phi)[0]
 

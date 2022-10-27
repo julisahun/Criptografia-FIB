2199cb220a0b525d14940fc032a16ce58c9cf774
julisahun <julisahun@gmail.com>
2022-10-27 11:33:57 +0200

    blockChain and test

--- P3/blockChain.py
+++ P3/blockChain.py
@@ -1,14 +1,68 @@
 import math
+import hashlib
 import sympy
 import random
 from tests import run_test
 
+d = 16
 class block:
     def __init__(self):
-        self.block_hash
-        self.previous_block_hash
-        self.transaction
-        self.seed
+        self.block_hash = 0
+        self.previous_block_hash = 0
+        self.transaction = transaction(150, rsa_key())
+        self.seed = 12
+    
+    def genesis(self, transaction):
+        self.transaction = transaction
+        self.previous_block_hash = 0
+        self.getBlockHash(transaction)
+
+    def next_block(self, transaction):
+        self.transaction = transaction
+        self.previous_block_hash = self.block_hash
+        self.getBlockHash(transaction)
+    
+    def verify_block(self):
+        prevCheck = self.previous_block_hash < 2**(256-d)
+        transCheck = self.transaction.verify()
+        currCheck = self.block_hash < 2**(256-d) and self.getHash(self.seed, self.transaction) == self.block_hash
+        return prevCheck and transCheck and currCheck
+    
+    def getBlockHash(self, transaction):
+        seed = 0
+        h = self.getHash(seed, transaction)
+        while h >= 2**(256-d):
+            seed = seed + 1
+            h = self.getHash(seed, transaction)
+        self.seed = seed
+        self.block_hash = h
+
+    def getHash(self, seed, transaction):
+        e = str(self.previous_block_hash)
+        e += str(self.transaction.public_key.publicExponent)
+        e += str(self.transaction.public_key.modulus)
+        e += str(self.transaction.message)
+        e += str(self.transaction.signature)
+        e += str(seed)
+        return int(hashlib.sha256(e.encode()).hexdigest(), 16)
+
+class block_chain:
+    def __init__(self, transaction):
+        self.list_of_blocks = [block().genesis(transaction)]
+
+    def add_block(self, transaction):
+        newBlock = self.list_of_blocks[-1].next_block(transaction)
+        self.list_of_blocks.append(newBlock)
+    
+    def verify(self):
+        if self.list_of_blocks[0].previous_block_hash != 0:
+            return False
+        for i in range(len(self.list_of_blocks)):
+            if (not self.list_of_blocks[i].verify_block()):
+                return False
+            if i > 0 and self.list_of_blocks[i].previous_block_hash != self.list_of_blocks[i-1].block_hash:
+                return False
+        return True
 
 class transaction:
     def __init__(self, message, RSAkey):
@@ -17,7 +71,7 @@ class transaction:
         self.signature = RSAkey.sign(message)
     
     def verify(self):
-        return self.public_key.verify(message, signature)
+        return self.public_key.verify(self.message, self.signature)
 
 class rsa_key:
     def check_congruent(self,a,c,m,debug=True):
@@ -81,5 +135,11 @@ class rsa_public_key:
 a = rsa_key()
 b = rsa_public_key(a)
 t = transaction(150, a)
-
+# b = block()
+# bc = block_chain(t)
+# b.genesis(t)
 run_test(a, b, t)
+
+
+
+# x2 + (r+s)x + rs = 0
--- P3/tests.py
+++ P3/tests.py
@@ -1,3 +1,5 @@
+import pickle
+
 def signatureTest(message, signer, verifier):
     otherMessage = message + 1
     if not verifier.verify(message, signer.sign(message)) or verifier.verify(message, signer.sign(otherMessage)): 
@@ -11,9 +13,23 @@ def transactionTest(transaction):
     else:
         print('Transaction test passed')
     
+def testBlockChain():
+    with open('./files/Cadena_bloques_valida.block', 'rb') as validData:
+        validBc = pickle.load(validData)
+    with open('./files/Cadena_bloques_bloque_falso.block', 'rb') as invalidData:
+        invalidBc = pickle.load(invalidData)
+    with open('./files/Cadena_bloques_seed_falsa.block', 'rb') as invalidData2:
+        invalidBc2 = pickle.load(invalidData2)
+    with open('./files/Cadena_bloques_transaccion_falsa.block', 'rb') as invalidData3:
+        invalidBc3 = pickle.load(invalidData3)
+    if not validBc.verify() or invalidBc.verify() or invalidBc2.verify() or invalidBc3.verify():
+        print('Block chain test not passed')
+    else:
+        print('Block chain test passed')
+
 
 def run_test(a, b, t):
     signatureTest(150, a, b)
     transactionTest(t)
-
+    testBlockChain()
         

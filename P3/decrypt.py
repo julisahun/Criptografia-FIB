068946066d7426dfe695d54194a40d3d1969180f
juli.sahun <juli.sahun@estudiantat.upc.edu>
2022-10-30 16:51:47 +0100

    2.2 done

--- P3/decrypt.py
+++ P3/decrypt.py
@@ -1,51 +1,63 @@
 from itertools import product
+from operator import mod
 from Crypto.PublicKey import RSA
 import sympy
 
-# key_encoded='''-----BEGIN PUBLIC KEY-----
-# MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQByM12PLgtaKidKoXwNWXZO
-# 6PhiNKqTU9yUYv81WkTxdn09+reugyUJ7ZIZGQAfWVszAuIgiaMb6/Dl8NRFx754
-# oGIMs+37xeVS36RbpAehgJuEm6SmX3NEA4liLipInux8bBlRjxZG68BA/PsVp1W0
-# 18zkYft2bFf2FvghXdOy9UDo5s5aO+mqcwxcm2xzJhIQ916BP6I9rcfvId0mfzQg
-# JU/0iaLp/F8SkpPdmn2YFY7KV0P7qSX+22hsnJMZExQIPdHhs9hH9PA0qfWYt2Q7
-# 3NOgrqE9Qa4iLr/qHw/ftrG+jLGgDfCxRsuhvuUFIY6lhS0hgMTMpLKk45fFAhwR
-# AgMBAAE=
-# -----END PUBLIC KEY-----'''
 
-# pubkey = RSA.importKey(key_encoded)
-# modulus = pubkey.n
-# public_exponent = pubkey.e
+####################### 2.1 #######################
+key_encoded='''-----BEGIN PUBLIC KEY-----
+MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQByM12PLgtaKidKoXwNWXZO
+6PhiNKqTU9yUYv81WkTxdn09+reugyUJ7ZIZGQAfWVszAuIgiaMb6/Dl8NRFx754
+oGIMs+37xeVS36RbpAehgJuEm6SmX3NEA4liLipInux8bBlRjxZG68BA/PsVp1W0
+18zkYft2bFf2FvghXdOy9UDo5s5aO+mqcwxcm2xzJhIQ916BP6I9rcfvId0mfzQg
+JU/0iaLp/F8SkpPdmn2YFY7KV0P7qSX+22hsnJMZExQIPdHhs9hH9PA0qfWYt2Q7
+3NOgrqE9Qa4iLr/qHw/ftrG+jLGgDfCxRsuhvuUFIY6lhS0hgMTMpLKk45fFAhwR
+AgMBAAE=
+-----END PUBLIC KEY-----'''
+
+pubkey = RSA.importKey(key_encoded)
+modulus = pubkey.n
+public_exponent = pubkey.e
 
 
 import math
-# from glob import glob
+from glob import glob
 
-# p , q = 0, 0
+p , q = 0, 0
 
-# for filename in glob('./otherKeys/*pubkeyRSA_RW*.pem'):
-#     f = open(filename)
-#     key2 = f.read()
-#     pubkey2 = RSA.importKey(key2)
-#     modulusB = pubkey2.n
-#     g = math.gcd(modulus,modulusB)
-#     if g != 1:
-#         if not p:
-#             p = g
-#         else:
-#             q = g
-#             break
-# phi = (p-1)*(q-1)
-# private_exponent = sympy.gcdex(public_exponent,phi)[0]
+for filename in glob('./otherKeys/*pubkeyRSA_RW*.pem'):
+    f = open(filename)
+    key2 = f.read()
+    pubkey2 = RSA.importKey(key2)
+    modulusB = pubkey2.n
+    g = math.gcd(modulus,modulusB)
+    if g != 1:
+        if not p:
+            p = g
+        else:
+            q = g
+            break
+phi = (p-1)*(q-1)
+private_exponent = sympy.gcdex(public_exponent,phi)[0]
 
 
-# from Crypto.Cipher import PKCS1_OAEP
+privateA = RSA.construct((modulus, public_exponent, int(private_exponent)))
 
-# privateA = RSA.construct((modulus, public_exponent, int(private_exponent)))
+outputA = open('PrivateKey.pem', 'wb')
+outputA.write(privateA.exportKey('PEM'))
+outputA.close()
 
-# outputA = open('PrivateKey.pem', 'wb')
-# outputA.write(privateA.exportKey('PEM'))
-# outputA.close()
+## decrypt rsa 
+#
+# openssl rsautl -decrypt -inkey PrivateKey.pem -in pseudoFiles/juli.sahun_RSA_pseudo.enc -out aes.key
+#
+#
+## decrypt aes
+#
+# openssl enc -d -aes-128-cbc -pbkdf2 -kfile aes.key -in pseudoFiles/juli.sahun_AES_pseudo.enc -out pic
 
+
+####################### 2.2 #######################
 key = '''-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2hTnyuijq265D/sZbmP3
 5r5pPnqz73SlNocf7/DaULPHqo2EAv1XaBvuIzwqqUQ/i51vKvLCJX5jjuwCEHyk
@@ -63,31 +75,39 @@ public_exponent = pubkey.e
 def leng(n):
     return math.ceil(math.log2(n))
 
-offset = -1
-while offset < 0:
+offset = 0
+
+b = 512
+
+while offset < 1:
     offset += 1
-    high = modulus >> (512*3)
-    
-    high -= offset
-    low = modulus & (2**512-1)
-    mid = (modulus & ((2**1024-1) << 512)) >> 512
+
+    high = (modulus >> b*3) - offset
+    mid = (modulus & ((2**(2*b)-1) << b)) >> b
+    low = modulus & (2**b-1)
+
+    producte = (high << b) | low
+    suma = math.isqrt(mid - (low << b | high) + 2*producte + (offset << 2*b))
 
 
-    producte = (high << 512) | low
-    suma = math.isqrt(mid - ((low << 512) | high) + 2*producte)
-    # print(math.ceil(math.log2(producte*4)))
-    # print(math.ceil(math.log2(suma**2)))
-    # print(math.ceil(math.log2(abs(suma**2 - producte*4))))
-
-    lamda = suma**2 - 4*producte
-    if (lamda < 0): 
+    d = suma**2 - 4*producte
+    if (d < 0): 
         continue
-    print('si soy')
-    r = (suma + math.isqrt(lamda))//2
-    s = (suma - math.isqrt(lamda))//2
+    r = (suma + math.isqrt(d))//2
+    s = (suma - math.isqrt(d))//2
 
-    p = (r << 512) | s
-    q = (s << 512) | r
+    p = (r << b) | s
+    q = (s << b) | r
     if (p*q == modulus):
-        print('aki toi')
+        break
+
+
+phi = (p-1)*(q-1)
+private_exponent = sympy.gcdex(public_exponent,phi)[0]
+
+privateA = RSA.construct((modulus, public_exponent, int(private_exponent)))
+
+outputA = open('PrivateKey.pem', 'wb')
+outputA.write(privateA.exportKey('PEM'))
+outputA.close()
 
